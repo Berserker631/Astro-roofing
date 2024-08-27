80801bed9c125021538a6c8c75fb570dc570f37c
Ner Aguilar <ner.aguilar06@gmail.com>
2024-08-26 20:10:40 -0400

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import ImageSlider from "./ImageSlider";
 
 const App = () => {
   const slides = [
-    { url: "../public/1.jpg", title: "roof", order: 1},
+    { url: "https://euphonious-sprinkles-624dce.netlify.app/public/1.jpg", title: "roof", order: 1},
     { url: "../public/2.jpg", title: "roof", order: 2},
     { url: "../public/3.jpg", title: "roof", order: 3},
     { url: "../public/4.jpg", title: "roof", order: 4},
